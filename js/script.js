82afcb19028fe48f809c9724af8719f6460d42fd
Karoline Ribeiro <karolsansonove@gmail.com>
2021-11-04 15:31:38 -0300

    commit teste

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ function alertar() {
     alert("Desculpe! O envio de informações do formuário ainda não está pronto.\n"
         + "\nVocê pode entrar em contato comigo através do e-mail karolsansonove@gmail.com ou pelo meu Linkedin.\n"
         + "\nObrigada pela compreensão!")
+    
+    Location.reload()
 }
