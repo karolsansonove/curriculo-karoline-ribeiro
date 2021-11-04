bce9dcd58a6ce8d6fded7303cc5526c98dc613a1
Karoline Ribeiro <karolsansonove@gmail.com>
2021-11-04 17:00:26 -0300

    ajuste no arquivo js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function alertar() {
     alert("Desculpe! O envio de informações do formuário ainda não está pronto.\n"
-        + "\nVocê pode entrar em contato comigo através do e-mail karolsansonove@gmail.com ou pelo meu Linkedin.\n"
+        + "\nEntre em contato comigo através do e-mail karolsansonove@gmail.com ou Linkedin.\n"
         + "\nObrigada pela compreensão!")
 }
