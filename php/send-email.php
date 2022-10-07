7c390d4486ca15f3ef13a4896cdd51933ae9bf83
Karoline Ribeiro <karolsansonove@gmail.com>
2022-10-06 23:15:24 -0300

    adicionando funcionalidades de envio de mensagem via e-mail com php

---
+++ php/send-email.php
@@ -0,0 +1,24 @@
+<?php
+
+    if (isset($_POST['email']) && !empty($_POST['email'])) { // verificando se a variável foi iniciada e se não está vazia
+        $nome = addslashes($_POST['nome']);
+        $email = addslashes($_POST['email']);
+        $mensagem = addslashes($_POST['mensagem']);
+
+        $destino = "karolsansonove@gmail.com";
+        $assunto = "Form de Contato - Currículo";
+        $corpo = "Nome: ".$nome."\r\n"
+                    ."E-mail: ".$email."\r\n"
+                    ."Mensagem: ".$mensagem;
+        $header = "From:contato@karolribeiro.dev.br\r\n" // precisa ser o mesmo domínio do site
+                    ."Reply-To:".$email."\r\n" // responder para e-mail do form
+                    ."X=Mailer:PHP/".phpversion(); // versão do php
+
+        if(mail($destino,$assunto,$corpo,$header)) {
+            echo "E-mail enviado com sucesso!";
+        } else {
+            echo "Não foi possível enviar.";
+        }
+
+    }
+?>
